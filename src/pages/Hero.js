55c117a9eaec7ea6be48a8289af9d5a4513e9079
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-11-02 10:56:59 +0200

    fix_hero_useEffect+setError

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -8,28 +8,36 @@ function Hero() {
   const { id } = useParams();
   const [hero, setHero] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const theme = useTheme();
 
   useEffect(() => {
+    const fetchHero = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(`${apiURL}/${id}`);
+        const data = await response.json();
+        setHero(data);
+      } catch (error) {
+        setError("Error loading character");
+        console.error("Error loading character:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchHero();
   }, [id]);
-
-  const fetchHero = async () => {
-    try {
-      const response = await fetch(`${apiURL}/${id}`);
-      const data = await response.json();
-      setHero(data);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error loading character:", error);
-      setLoading(false);
-    }
-  };
 
   return (
     <Box sx={{ textAlign: "center", mt: 4, color: theme.palette.text.primary }}>
       {loading ? (
         <CircularProgress />
+      ) : error ? (
+        <Typography variant="body1" color="error">
+          {error}
+        </Typography>
       ) : (
         <Box>
           <img src={hero.image} alt={hero.name} width="100%" />
